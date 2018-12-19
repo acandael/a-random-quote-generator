64ff211b014f7ef4aacd6c5208664de629207ef0
acandael <info@anthonycandaele.com>
2018-12-19 16:19:55 +0100

    add quotes

---
+++ js/quotes.js
@@ -0,0 +1,26 @@
+quotes = [
+  {
+    quote: "Don't cry because it's over, smile because it happened.",
+    source: "Dr. Seuss"
+  },
+  {
+    quote: "Be yourself; everyone else is already taken.",
+    source: "Oscar Wilde"
+  },
+  {
+    quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+    source: "Albert Einstein"
+  },
+  {
+    quote: "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
+    source: "J.K. Rowling",
+    citation: "Harry Potter and the Goblet of Fire",
+    year: 2000
+  },
+  {
+    quote: "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.",
+    source: "Martin Luther King Jr.",
+    citation: "A Testament of Hope: The Essential Writings and Speeches"
+  }
+
+];
